8246d628a137df06a7c5b75ebbb048b70b1c07d1
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2019-10-04 18:03:49 +0300

    Updated copyright notice

--- src/main/java/eu/javaspecialists/books/dynamicproxies/shortcut/chap2/c_2_3/bank/Konto.java
+++ src/main/java/eu/javaspecialists/books/dynamicproxies/shortcut/chap2/c_2_3/bank/Konto.java
@@ -1,20 +1,19 @@
 /*
- * Copyright (c) 2014. Heinz Max Kabutz , Sven Ruppert
- *   We licenses
- *   this file to you under the Apache License, Version 2.0 (the
- * "License");
- *   you may not use this file except in compliance with the License.
- * You may
- *   obtain a copy of the License at
+ * Copyright (C) 2000-2019 Heinz Max Kabutz
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.  Heinz Max Kabutz licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
  *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
- * implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package eu.javaspecialists.books.dynamicproxies.shortcut.chap2.c_2_3.bank;
