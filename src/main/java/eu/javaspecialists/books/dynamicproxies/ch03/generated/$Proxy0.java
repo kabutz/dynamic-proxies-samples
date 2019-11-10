2d86c80b382cffbdcf46cf771bddab2d7f0788e3
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2019-11-10 18:32:52 +0100

    Demo of what a generated dynamic proxy looks like

---
+++ src/main/java/eu/javaspecialists/books/dynamicproxies/ch03/generated/$Proxy0.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2000-2019 Heinz Max Kabutz
+ *
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.  Heinz Max Kabutz
+ * licenses this file to you under the Apache License, Version
+ * 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the
+ * License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.  See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ */
+
+package eu.javaspecialists.books.dynamicproxies.ch03.generated;
+
+// tag::listing[]
+import eu.javaspecialists.books.dynamicproxies.ch03.*;
+
+import java.lang.reflect.*;
+
+public final class $Proxy0 extends Proxy implements Subject {
+  private static Method m0;
+  private static Method m1;
+  private static Method m2;
+  private static Method m3;
+
+  public $Proxy0(InvocationHandler h) {
+    super(h);
+  }
+
+  public final int hashCode() {
+    try {
+      return (Integer) h.invoke(this, m0, (Object[]) null);
+    } catch (RuntimeException | Error e) {
+      throw e;
+    } catch (Throwable e) {
+      throw new UndeclaredThrowableException(e);
+    }
+  }
+
+  public final boolean equals(Object o) {
+    try {
+      return (Boolean) h.invoke(this, m1, new Object[] {o});
+    } catch (RuntimeException | Error e) {
+      throw e;
+    } catch (Throwable e) {
+      throw new UndeclaredThrowableException(e);
+    }
+  }
+
+  public final String toString() {
+    try {
+      return (String) h.invoke(this, m2, (Object[]) null);
+    } catch (RuntimeException | Error e) {
+      throw e;
+    } catch (Throwable e) {
+      throw new UndeclaredThrowableException(e);
+    }
+  }
+
+  public final String uppercaseTrim(String s) {
+    try {
+      return (String) h.invoke(this, m3, new Object[] {s});
+    } catch (RuntimeException | Error e) {
+      throw e;
+    } catch (Throwable e) {
+      throw new UndeclaredThrowableException(e);
+    }
+  }
+
+  static {
+    try {
+      m0 = Class.forName("java.lang.Object")
+               .getMethod("hashCode");
+      m1 = Class.forName("java.lang.Object")
+               .getMethod("equals",
+                   Class.forName("java.lang.Object"));
+      m2 = Class.forName("java.lang.Object")
+               .getMethod("toString");
+      m3 = Class.forName(
+          "eu.javaspecialists.books.dynamicproxies.ch03.Subject")
+               .getMethod("uppercaseTrim",
+                   Class.forName("java.lang.String"));
+    } catch (NoSuchMethodException e) {
+      throw new NoSuchMethodError(e.getMessage());
+    } catch (ClassNotFoundException e) {
+      throw new NoClassDefFoundError(e.getMessage());
+    }
+  }
+}
+// end::listing[]
+
+
+
