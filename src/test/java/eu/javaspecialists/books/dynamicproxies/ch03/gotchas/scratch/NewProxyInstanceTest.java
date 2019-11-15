c1468ffc56ea6f2583d22721b0eb45f0fa9530e6
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2019-11-15 17:42:53 +0200

    Created unit test for virtual proxy and newProxyInstance() calls

--- src/test/java/eu/javaspecialists/books/dynamicproxies/ch03/gotchas/scratch/NewProxyInstanceTest.java
+++ src/test/java/eu/javaspecialists/books/dynamicproxies/ch03/gotchas/scratch/NewProxyInstanceTest.java
@@ -20,13 +20,14 @@
 
 package eu.javaspecialists.books.dynamicproxies.ch03.gotchas.scratch;
 
+import eu.javaspecialists.books.dynamicproxies.*;
 import org.junit.*;
 
 import java.lang.reflect.*;
 
 import static org.junit.Assert.*;
 
-public class InvalidProxyCalls {
+public class NewProxyInstanceTest {
   private final static InvocationHandler UNSUPPORTED =
       (proxy, method, args) -> {
         throw new UnsupportedOperationException("not implemented");
@@ -34,15 +35,15 @@ public class InvalidProxyCalls {
 
   @Test(expected = IllegalArgumentException.class)
   public void classIsString() {
-    castProxy(String.class, UNSUPPORTED);
+    Proxies.castProxy(String.class, UNSUPPORTED);
   }
   @Test(expected = IllegalArgumentException.class)
   public void classIsPrimitive() {
-    castProxy(int.class, UNSUPPORTED);
+    Proxies.castProxy(int.class, UNSUPPORTED);
   }
   @Test(expected = IllegalArgumentException.class)
   public void classIsAbstractNumber() {
-    castProxy(Number.class, UNSUPPORTED);
+    Proxies.castProxy(Number.class, UNSUPPORTED);
   }
   @Test(expected = IllegalArgumentException.class)
   public void duplicateClasses() {
@@ -66,7 +67,7 @@ public class InvalidProxyCalls {
         proxy.getClass().getPackageName());
   }
 
-  @Test
+  @Test(expected = IllegalArgumentException.class)
   public void twoPackageAccessInterfaces() throws ClassNotFoundException {
     Class<?> clazz = Class.forName(
         getClass().getPackageName() + ".hidden.Foo");
@@ -89,8 +90,8 @@ public class InvalidProxyCalls {
     System.out.println(proxy.getClass().getClassLoader());
   }
 
-  @Test
-  public void testDifferentClassLoaders() throws ClassNotFoundException {
+  @Test(expected = IllegalArgumentException.class)
+  public void invisibleInterface() throws ClassNotFoundException {
     Object proxy = Proxy.newProxyInstance(
         Runnable.class.getClassLoader(),
         new Class<?>[] {Foo.class},
@@ -99,8 +100,9 @@ public class InvalidProxyCalls {
     System.out.println(proxy.getClass());
     System.out.println(proxy.getClass().getPackageName());
   }
+
   @Test
-  public void testDifferentClassLoaders2() throws ClassNotFoundException {
+  public void visibleInterface() throws ClassNotFoundException {
     Object proxy = Proxy.newProxyInstance(
         Foo.class.getClassLoader(),
         new Class<?>[] {Runnable.class},
@@ -125,61 +127,25 @@ public class InvalidProxyCalls {
     System.out.println(Runnable.class.getClassLoader());
   }
 
-  private static <P> P castProxy(Class<P> proxiedInterface,
-                                 InvocationHandler handler) {
-    return proxiedInterface.cast(Proxy.newProxyInstance(
-        proxiedInterface.getClassLoader(),
-        new Class<?>[] {proxiedInterface}, handler
-    ));
-  }
+  @Test
+  public void packagePrivatePackageBootClassPath()
+      throws ClassNotFoundException {
+    // package private class
+    Class<?> intf = Class.forName("java.util.stream.Sink");
+    InvocationHandler h = new InvocationHandler() {
+      @Override
+      public Object invoke(Object proxy, Method method,
+                           Object[] args) throws Throwable {
+        System.out.println("Hackity.invoke");
+        return null;
+      }
+    };
+    Object proxy = Proxies.castProxy(intf, h);
 
-  public static void main(String... args) {
-
-    /* <p>
-     * <li>All of the interface types must be visible by name
-     * through the
-     * specified class loader. In other words, for class loader
-     * {@code cl} and every interface {@code i}, the following
-     * expression must be true:<p>
-     * {@code Class.forName(i.getName(), false, cl) == i}
-     *
-     * <li>All of the types referenced by all
-     * public method signatures of the specified interfaces
-     * and those inherited by their superinterfaces
-     * must be visible by name through the specified class loader.
-     *
-     * <li>All non-public interfaces must be in the same package
-     * and module, defined by the specified class loader and
-     * the module of the non-public interfaces can access all of
-     * the interface types; otherwise, it would not be possible for
-     * the proxy class to implement all of the interfaces,
-     * regardless of what package it is defined in.
-     *
-     * <li>For any set of member methods of the specified interfaces
-     * that have the same signature:
-     * <ul>
-     * <li>If the return type of any of the methods is a primitive
-     * type or void, then all of the methods must have that same
-     * return type.
-     * <li>Otherwise, one of the methods must have a return type that
-     * is assignable to all of the return types of the rest of the
-     * methods.
-     * </ul>
-     *
-     * <li>The resulting proxy class must not exceed any limits imposed
-     * on classes by the virtual machine.  For example, the VM may
-     * limit
-     * the number of interfaces that a class may implement to 65535; in
-     * that case, the size of the {@code interfaces} array must not
-     * exceed 65535.
-     * </ul>
-     *
-     * <p>Note that the order of the specified proxy interfaces is
-     * significant: two requests for a proxy class with the same
-     * combination
-     * of interfaces but in a different order will result in two
-     * distinct
-     * proxy classes.
-     */
+    String intfPackageName = intf.getPackageName();
+    String proxyPackageName = proxy.getClass().getPackageName();
+    assertEquals("java.util.stream", intfPackageName);
+    assertEquals("java.util.stream", proxyPackageName);
+    assertNull(proxy.getClass().getClassLoader());
   }
 }
