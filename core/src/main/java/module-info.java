a78e09d51bbc1247142864b45ac805e9cdf8055a
John Green <john@javaspecialists.eu>
2020-01-27 14:04:38 +0000

    Remove exports of internal packages

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -7,19 +7,6 @@ module eu.javaspecialists.books.dynamicproxies {
   exports eu.javaspecialists.books.dynamicproxies;
   exports eu.javaspecialists.books.dynamicproxies.util;
   exports eu.javaspecialists.books.dynamicproxies.util.chain;
-  exports eu.javaspecialists.books.dynamicproxies.ch02.equals;
-  exports eu.javaspecialists.books.dynamicproxies.ch02.protection;
-  exports eu.javaspecialists.books.dynamicproxies.ch03.enhancedstream;
-  exports eu.javaspecialists.books.dynamicproxies.ch03.gotchas;
-  exports eu.javaspecialists.books.dynamicproxies.ch03.protection;
-  exports eu.javaspecialists.books.dynamicproxies.ch03.virtual;
-  exports eu.javaspecialists.books.dynamicproxies.ch04;
-  exports eu.javaspecialists.books.dynamicproxies.ch04.immutablecollection;
-  exports eu.javaspecialists.books.dynamicproxies.ch05;
-  exports eu.javaspecialists.books.dynamicproxies.ch05.bettercollection;
-  exports eu.javaspecialists.books.dynamicproxies.ch06;
-  exports eu.javaspecialists.books.dynamicproxies.ch06.appendables;
-  exports eu.javaspecialists.books.dynamicproxies.ch06.contact;
 
   // please let us know if we need to export more by emailing
   // heinz@javaspecialists.eu
